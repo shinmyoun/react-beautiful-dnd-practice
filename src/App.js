6d32d41c709c983f7b21616d531d78bc73a1b9d6
shinmyou <takuya.shinmyou@hybitz.co.jp>
2022-11-06 10:53:37 +0900

    ✨ ドロップ後の並び替えを実装

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
+import { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import "./App.css";
 
 function App() {
+  const [items, setItem] = useState(["item0", "item1", "item2"]);
+
+  const onDragEnd = (result) => {
+    console.log(result.source.index);
+    console.log(result.destination.index);
+    const remove = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, remove[0]);
+    console.log(items);
+  };
   return (
-    <div>
-      <DragDropContext>
+    <div className="dragDropArea">
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
               <Draggable draggableId="item0" index={0}>
                 {(provided) => (
                   <div
+                    className="item"
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                   >
-                    item0
+                    {items[0]}
                   </div>
                 )}
               </Draggable>
+              <Draggable draggableId="item1" index={1}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[1]}
+                  </div>
+                )}
+              </Draggable>
+              <Draggable draggableId="item2" index={2}>
+                {(provided) => (
+                  <div
+                    className="item"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    {items[2]}
+                  </div>
+                )}
+              </Draggable>
+              {provided.placeholder}
             </div>
           )}
         </Droppable>
